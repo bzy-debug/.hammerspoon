b4b8d8b654bb4d1cc73dafb182fe808b3c82480e
Bao Zhiyuan <baozhiyuan@idea.edu.cn>
2025-11-21 14:42:39 +0800

    Remove 'com.apple.mail' window detection rule and improve install script to clean up downloaded zip files

--- install.sh
+++ install.sh
@@ -5,8 +5,14 @@ mkdir -p ./Spoons
 spoons=("BingDaily" "EmmyLua" "LeftRightHotkey")
 
 for spoon in "${spoons[@]}"; do
+  if [ -d "./Spoons/${spoon}.spoon" ]; then
+    echo "${spoon}.spoon is already installed, skipping..."
+    continue
+  fi
+
   wget "https://github.com/Hammerspoon/Spoons/raw/master/Spoons/${spoon}.spoon.zip"
   unzip "${spoon}.spoon.zip" -d ./Spoons/
+  rm "${spoon}.spoon.zip"
 done
 
 ln -sf ./.aerospace.toml ~/.aerospace.toml
